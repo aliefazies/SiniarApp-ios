13b0edf3c1f11a70e1779e739e8a4deb1259c3f8
Alief Azies <alief.ahmadazies@gmail.com>
2023-09-19 11:17:04 +0700

    Initial Commit

---
+++ SiniarApp-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SiniarApp-ios
+//
+//  Created by Alief Ahmad Azies on 19/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
